a9a087821950fcdd5c8797591c9974986e36e0f0
doubleface <christophe@cozycloud.cc>
2018-11-21 08:13:38 +0100

    fix: remove svg optimization which breaks the image

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,8 @@
 var path = require('path')
 const CopyPlugin = require('copy-webpack-plugin')
 const fs = require('fs')
-const SvgoInstance = require('svgo')
 
 const entry = require('./package.json').main
-
-const svgo = new SvgoInstance()
 
 let iconName
 try {
@@ -15,7 +12,6 @@ try {
 } catch (e) {
     console.error(`Unable to read the icon path from manifest: ${e}`)
 }
-const appIconRX = iconName && new RegExp(`[^/]*/${iconName}`)
 
 module.exports = {
     entry,
@@ -30,17 +26,9 @@ module.exports = {
             { from: 'manifest.konnector' },
             { from: 'package.json' },
             { from: 'README.md' },
-            { from: 'assets', transform: optimizeSVGIcon },
+            { from: 'assets' },
             { from: '.travis.yml' },
             { from: 'LICENSE' }
         ])
     ]
-}
-
-function optimizeSVGIcon(buffer, path) {
-    if (appIconRX && path.match(appIconRX)) {
-        return svgo.optimize(buffer).then(resp => resp.data)
-    } else {
-        return buffer
-    }
 }
