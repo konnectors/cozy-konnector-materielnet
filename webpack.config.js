c52f377620f4b93674b44fbe90046e9f72296896
LucsT <lucas@cozycloud.cc>
2022-02-24 17:47:37 +0100

    packaging: Add build files

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,55 @@
 var path = require('path')
 const CopyPlugin = require('copy-webpack-plugin')
+const webpack = require('webpack')
 const fs = require('fs')
 const SvgoInstance = require('svgo')
 
 const entry = require('./package.json').main
 
+const readManifest = () =>
+  JSON.parse(fs.readFileSync(path.join(__dirname, './manifest.konnector')))
+
 const svgo = new SvgoInstance({
   plugins: [
     {
-      inlineStyles: {
-        onlyMatchedOnce: false
-      }
+      inlineStyles: { onlyMatchedOnce: false }
     }
   ]
 })
 
 let iconName
 try {
-    iconName = JSON.parse(fs.readFileSync('manifest.konnector', 'utf8')).icon
-    // we run optimize only on SVG
-    if (!iconName.match(/\.svg$/)) iconName = null
+  iconName = JSON.parse(fs.readFileSync('manifest.konnector', 'utf8')).icon
+  // we run optimize only on SVG
+  if (!iconName.match(/\.svg$/)) iconName = null
 } catch (e) {
-    console.error(`Unable to read the icon path from manifest: ${e}`)
+  // console.error(`Unable to read the icon path from manifest: ${e}`)
 }
 const appIconRX = iconName && new RegExp(`[^/]*/${iconName}`)
 
 module.exports = {
-    entry,
-    target: 'node',
-    mode: 'none',
-    output: {
-        path: path.join(__dirname, 'build'),
-        filename: 'index.js'
-    },
-    plugins: [
-        new CopyPlugin([
-            { from: 'manifest.konnector' },
-            { from: 'package.json' },
-            { from: 'README.md' },
-            { from: 'assets', transform: optimizeSVGIcon },
-            { from: '.travis.yml' },
-            { from: 'LICENSE' }
-        ])
-    ],
+  entry,
+  target: 'node',
+  mode: 'none',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'index.js'
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'manifest.konnector' },
+        { from: 'package.json' },
+        { from: 'README.md' },
+        { from: 'assets', transform: optimizeSVGIcon },
+        { from: '.travis.yml' },
+        { from: 'LICENSE' }
+      ]
+    }),
+    new webpack.DefinePlugin({
+      __WEBPACK_PROVIDED_MANIFEST__: JSON.stringify(readManifest())
+    })
+  ],
   module: {
     // to ignore the warnings like :
     // WARNING in ../libs/node_modules/bindings/bindings.js 76:22-40
